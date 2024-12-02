6c09563e4cfd262c43c0d8bdb3c4e8c5112b73e3
Samuel <sassani134@gmail.com>
2024-12-02 11:07:40 +0100

    Add migration to Game model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_01_235051) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_02_095413) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -50,6 +50,12 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_01_235051) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "mobility_string_translations", force: :cascade do |t|
