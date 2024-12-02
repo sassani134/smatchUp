b42b68a3f711b07038648d48649e1b1a861ed77f
Samuel <sassani134@gmail.com>
2024-12-02 12:24:39 +0100

    Add rich text

--- app/models/matchup_post.rb
+++ app/models/matchup_post.rb
@@ -1,3 +1,13 @@
 class MatchupPost < ApplicationRecord
   belongs_to :user
+  has_rich_text :victory_conditions
+  has_rich_text :defeat_condition
+  has_rich_text :neutral
+  has_rich_text :disadvantage_edge
+  has_rich_text :disadvantage_offstage
+  has_rich_text :disadvantage_juggle
+  has_rich_text :advantage_edge
+  has_rich_text :advantage_offstage
+  has_rich_text :advantage_juggle
+  has_rich_text :miscellaneou
 end
